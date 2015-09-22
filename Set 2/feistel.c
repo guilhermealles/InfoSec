af030dd75b47c3221bfb47e573cfc7ab31e5833a
Isadora Pedrini Possebon <Isadora@Isadoras-MacBook-Pro.local>
2015-09-22 02:40:36 +0200

    aaa

--- Set 2/feistel.c
+++ Set 2/feistel.c
@@ -6,24 +6,33 @@ typedef char byte;
 
 int getSubkey(int round);
 void populate_subkeys();
+int f(int block, int subkey);
+void feistelEncrypt();
+void feistelDecrypt();
 
-char *plain_text;
-int plain_text_size;
+char input_text[] = {97, 109, 111, 114, -69, -29, 32, 12, '\0'};
+//char* input_text;
+int input_text_size;
 char *passphrase;
 char expanded_passphrase_1[] = "184b4d16bbe3200c5a5f500cc09efa68cddd42cbda27c1e49fa7a0f2e2735007";
 char expanded_passphrase_2[] = "bd11fd28eabd0b87f2ff4595a50041bfb882bbf8ae058ea5d677c7da07d43786";
 byte *subkeys;
+char* output_text;
 
 int main (int argc, char **argv) {
-    plain_text = argv[1];
+    //input_text = argv[1];
     passphrase = argv[2];
-    plain_text_size = strlen(plain_text);
+    input_text_size = strlen(input_text);
+    output_text = malloc(sizeof(char)*input_text_size);
 
     subkeys = malloc(sizeof(byte) * 4 * 16);
     populate_subkeys();
 
-
-    printf("Subkey: %04x\n", getSubkey(15));
+    feistelDecrypt();
+    for (int i = 0; i < input_text_size; i++) {
+        printf("%d ", (char)output_text[i]);
+    }
+    printf("\n");
 
     free(subkeys);
     return 1;
@@ -92,26 +101,50 @@ int getSubkey(int round) {
     return return_value;
 }
 
-void feistel() {
-    int current_round = 0;
-    for (current_round = 0; current_round < 16; current_round++) {
-        int i = 0;
-        for (i=0; i<plain_text_size; i+=8) {
-            // i is the index for the next block;
-            byte L[4], R[4];
-            int j
-            for (j = 0; j < 4; j++) {
-                L[j] = plain_text[i];
-                R[j] = plain_text[i+4];
-            }
+void feistelEncrypt() {
+    int R[16];
+    int L[16];
+
+    // For each block in the plain text.
+    for (int i = 0; i < input_text_size; i+=8){
+        int* L_ptr = (int*)&input_text[i];
+        int* R_ptr = (int*)&input_text[i+4];
+
+        L[0] = *L_ptr;
+        R[0] = *R_ptr;
+        for (int current_round = 1; current_round < 16; current_round++){ //16
+            L[current_round] = R[current_round-1];
+            R[current_round] = L[current_round-1] ^ f(R[current_round-1], getSubkey(current_round));
         }
+        L_ptr = (int*)&output_text[i];
+        R_ptr = (int*)&output_text[i+4];
+        *L_ptr = L[15];
+        *R_ptr = R[15];
+    }
+}
+
+void feistelDecrypt(){
+    int R[16];
+    int L[16];
+
+    for (int i = 0; i < input_text_size; i++){
+        int* L_ptr = (int*)&input_text[i];
+        int* R_ptr = (int*)&input_text[i+4];
+
+        L[15] = *L_ptr;
+        R[15] = *R_ptr;
+        for (int current_round = 15; current_round > 0; current_round--){
+            R[current_round-1] = L[current_round];
+            L[current_round-1] = R[current_round] ^ f(R[current_round-1], getSubkey(current_round));
+        }
+        L_ptr = (int*)&output_text[i];
+        R_ptr = (int*)&output_text[i+4];
+        *L_ptr = L[0];
+        *R_ptr = R[0];
     }
 }
 
 // Assuming int = 4 bytes and block is a 4 bytes array
-int f(byte *block, int subkey) {
-    int *blockPtr = block;
-    int blockValue = *blockptr;
-
-    return blockValue ^ subkey;
+int f(int block, int subkey) {
+    return block ^ subkey;
 }
